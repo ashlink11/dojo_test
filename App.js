ed6b9df1ec007aed1bc28f6deb6beca0687c2973
Ash Grevelink <ashlink1111@gmail.com>
2020-06-10 23:22:43 -0400

    Add React index.js and App.js files

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <div>
+        <h1>A React Web App Template</h1>
+        <p>Built with yarn & Parcel</p>
+      </div>
+      <p>Copyright 2020 - Your name</p>
+    </div>
+  );
+}
